05d68933a9009801eb71c57559b48ab2de4b53db
Mustard-Zeit <137809930+Mustard-Zeit@users.noreply.github.com>
2024-01-25 07:22:13 +0000

    Pending changes exported from your codespace

--- login.js
+++ login.js
@@ -5,3 +5,32 @@ document.addEventListener('DOMContentLoeaded',function(){
   console.log(inputEmail);
   console.log(inputPassword);
 });
+
+document.addEventListener('DOMContentLoaded', function(){
+  console-log("Hello World!");
+  var inputEmail = this.getElementById('email');
+  var inputPassword = this.getElementById('password');
+  console.log(inputEmail);
+  console.log(inputPassword);
+
+});
+
+//Validacion de email//
+
+function validarEmail(email) {
+
+  var patron = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+
+
+  return patron.test(email);
+}
+
+
+var emailValido = "usuario@dominio.com";
+var emailInvalido = "usuario@dominio";
+
+console.log(validarEmail(emailValido));
+console.log(validarEmail(emailInvalido));
+
+//Validacion de Password//
+
